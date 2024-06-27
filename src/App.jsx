6e7ad13cd5894907574d1fc7671119b00f23e97c
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-06-27 23:33:14 +0200

    Début de liste d'images et d'affichage dans le canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+/* eslint-disable react/no-unknown-property */
+import { Canvas, useLoader } from "@react-three/fiber";
+import { TextureLoader } from "three";
+
+import creativeImages from "./content/CreativeImages";
+
 function App() {
+  const texture = useLoader(TextureLoader, creativeImages[0].image);
+
   return (
     <>
       <div className="h-screen w-screen relative bg-gray-100">
-        <h1>Mouse Image Gallery</h1>
+        <Canvas>
+          <ambientLight intensity={3} />
+          <mesh>
+            <planeGeometry args={[1, 1]} />
+            <meshBasicMaterial map={texture} />
+          </mesh>
+        </Canvas>
       </div>
     </>
   );
---
+++ src/content/CreativeImages.js
@@ -0,0 +1,9 @@
+const creativeImages = [
+  {
+    name: "Arta Jens",
+    twitter: "@artajens",
+    image: "/img/artajens.webp",
+  },
+];
+
+export default creativeImages;
