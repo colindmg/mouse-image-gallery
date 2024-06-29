44928081f4043bac64738506734d5e6f465061fe
Colin Demouge <83725537+colindmg@users.noreply.github.com>
2024-06-29 20:34:36 +0200

    Animation d'apparition en opacity du @ de l'image selectionnée

--- src/components/DynamicImageInfos.jsx
+++ src/components/DynamicImageInfos.jsx
@@ -1,9 +1,52 @@
 import { Text } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 import PropTypes from "prop-types";
+import { useEffect, useRef, useState } from "react";
 
 const DynamicImageInfos = ({ twitter, position, imageScale, isVisible }) => {
+  const textRef = useRef(null);
+  const [opacity, setOpacity] = useState(isVisible ? 1 : 0);
+
+  // Mettre à jour l'opacité progressivement lorsque isVisible change
+  useEffect(() => {
+    if (isVisible) {
+      let opacityValue = 0;
+      const opacityIncrement = 0.1;
+      const fadeInInterval = setInterval(() => {
+        opacityValue += opacityIncrement;
+        if (opacityValue >= 1) {
+          opacityValue = 1;
+          clearInterval(fadeInInterval);
+        }
+        setOpacity(opacityValue);
+      }, 50);
+      return () => clearInterval(fadeInInterval);
+    } else {
+      let opacityValue = 1;
+      const opacityDecrement = 0.1;
+      const fadeOutInterval = setInterval(() => {
+        opacityValue -= opacityDecrement;
+        if (opacityValue <= 0) {
+          opacityValue = 0;
+          clearInterval(fadeOutInterval);
+        }
+        setOpacity(opacityValue);
+      }, 50);
+      return () => clearInterval(fadeOutInterval);
+    }
+  }, [isVisible]);
+
+  // Mettre à jour l'opacité du texte à chaque frame
+  useFrame(() => {
+    if (textRef.current) {
+      textRef.current.material.opacity = opacity;
+      textRef.current.needsUpdate = true;
+    }
+  });
+
   return (
     <Text
+      ref={textRef}
       color="black"
       fontSize={0.2}
       position={[
@@ -11,7 +54,7 @@ const DynamicImageInfos = ({ twitter, position, imageScale, isVisible }) => {
         position[1] - 1.7 * imageScale,
         position[2],
       ]}
-      fillOpacity={isVisible ? 1 : 0}
+      fillOpacity={opacity}
     >
       {twitter}
     </Text>
